01f29b533382dfd787a4166170ad8745774a53dc
Michael Stapelberg <michael@stapelberg.de>
2017-05-01 16:53:14 +0200

    add forgotten bundle.go for go generate

---
+++ bundle.go
@@ -0,0 +1,3 @@
+package main
+
+//go:generate sh -c "go run goembed.go -package bundled -var assets assets/elements.vulcanized.html assets/scan2drive.js assets/drive48.png > internal/bundled/GENERATED_bundled.go"
