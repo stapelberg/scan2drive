9f07348e676df8db33ca5ec90ba25269a2df6358
Michael Stapelberg <michael@stapelberg.de>
2017-05-01 17:29:32 +0200

    fix typo in license header

--- internal/fss500/fss500.go
+++ internal/fss500/fss500.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./
+// limitations under the License.
 
 // Package fss500 is a driver for the Fujitsu ScanSnap iX500 document
 // scanner, implemented from scratch based on USB traffic
