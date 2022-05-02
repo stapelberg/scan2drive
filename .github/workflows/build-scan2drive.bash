6698df4c3a95746d21a1445da4fd06856d59bfed
Michael Stapelberg <michael@stapelberg.de>
2022-05-02 19:36:54 +0200

    actually install the scan2drive command

--- .github/workflows/build-scan2drive.bash
+++ .github/workflows/build-scan2drive.bash
@@ -2,5 +2,8 @@
 
 set -x
 
+cd $(mktemp -d)
+git clone /usr/src/scan2drive
+cd scan2drive
 export PATH=$PWD/_bundled_turbojpeg:$PATH
-GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go install -ldflags='-linkmode external -extldflags -static' -tags turbojpeg github.com/stapelberg/scan2drive
+GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go install '-ldflags=-linkmode external -extldflags -static' -tags turbojpeg -v ./cmd/scan2drive
